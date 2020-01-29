ae7c5ecdccb388369efb6cec6e0ee3f1e9e00fa1
Ben Wright <ben1991wright@hotmail.co.uk>
2020-01-29 16:51:20 +0000

    add admin user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating admin user'
+
+User.create(email: 'test@test.com', password: '123123', admin: true)
+
+puts 'Done'
