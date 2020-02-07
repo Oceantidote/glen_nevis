4fdef7a03d4603e993f6538b74fc231d3723b3aa
leonard percival <leonardpercival@gmail.com>
2020-02-07 17:49:32 +0000

    price now updates with multiple days, taking into account whether or not the booking is on a weekend or has seasonal price fluctuations

--- app/javascript/components/form/priceHandler.js
+++ app/javascript/components/form/priceHandler.js
@@ -1,5 +1,7 @@
 import { anytimeHeaders } from './anytimeHeaders'
 import { priceFormatter } from './priceFormatter'
+import moment from 'moment'
+import twix from 'twix'
 
 export const priceHandler = () => {
   $('#unit-dropdown').change(async e => {
@@ -11,8 +13,24 @@ export const priceHandler = () => {
       })
     })
     const data = await response.json()
-    document.getElementById('base').innerHTML = priceFormatter.format(data[0]['rate'])
-    $('#base').val(data[0]['rate']*100)
+    const arrival = moment($('#arrival_date').val(), 'DD/MM/YYYY').format(
+      'YYYY-MM-DD'
+    )
+    const departure = $('#departure_date').val()
+    var itr = moment.twix(new Date(arrival),new Date(departure)).iterate("days");
+    var range=[];
+    while(itr.hasNext()){
+        range.push(itr.next().toDate())
+    }
+    range.shift()
+    let prices = []
+    range.forEach(date => {
+      let price = data.filter( x => new Date(x['day']) <= new Date(date) && new Date(x['day2']) >= new Date(date))
+      prices.push(price[0]['rate'])
+    })
+    let sum = prices.reduce((a, b) => a + b, 0)
+    document.getElementById('base').innerHTML = priceFormatter.format(sum)
+    $('#base').val(sum*100)
   })
 }
 
