142aa808b4a12adf208b8c5a9d24fdb6bd5b5621
leonard percival <leonardpercival@gmail.com>
2020-02-10 17:23:55 +0000

    removed logs

--- app/javascript/components/form/postcodeButtonEnabler.js
+++ app/javascript/components/form/postcodeButtonEnabler.js
@@ -2,22 +2,18 @@ export const postcodeButtonEnabler = () => {
   $("#postcode").keyup(e => {
     document.getElementById('address-holder').innerHTML = ""
     if (e.currentTarget.value.length >= 5 ) {
-      console.log("enable")
       document.getElementById('find_address').classList.remove("disabled")
     }
     if (e.currentTarget.value.length < 5 ) {
-      console.log("disable")
       document.getElementById('find_address').classList.add("disabled")
     }
   })
   $("#postcode").change(e => {
     document.getElementById('address-holder').innerHTML = ""
     if (e.currentTarget.value.length >= 5 ) {
-      console.log("enable")
       document.getElementById('find_address').classList.remove("disabled")
     }
     if (e.currentTarget.value.length < 5 ) {
-      console.log("disable")
       document.getElementById('find_address').classList.add("disabled")
     }
   })
