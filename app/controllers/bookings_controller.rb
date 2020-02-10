79ddb606797ae6e0023badcda064142676c8a3af
leonard percival <leonardpercival@gmail.com>
2020-02-10 15:21:04 +0000

    remove raise

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,7 +19,6 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.extras = convert_extras
-    raise
     if @booking.save
       if params.dig(:booking, :print) == 'true'
         redirect_to print_booking_path(@booking)
