bc10480fd761e667ecb5cea07761143e003f10a5
leonard percival <leonardpercival@gmail.com>
2020-02-11 13:37:37 +0000

    removed val on subunit

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   before_create :upcase_vehicle_reg
-  validates :arrival, :departure, :first_name, :last_name, :unit_id, :email, :postcode, :address1, :country, :base_cents, :party_cents, :add_on_cents, :discount_cents, :price_cents, :subunit_id, presence: true
+  validates :arrival, :departure, :first_name, :last_name, :unit_id, :email, :postcode, :address1, :country, :base_cents, :party_cents, :add_on_cents, :discount_cents, :price_cents, presence: true
   validates :gdpr, acceptance: true
   def upcase_vehicle_reg
     self[:vehicle_reg] = vehicle_reg.upcase
