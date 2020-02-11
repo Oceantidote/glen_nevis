de2c7725aead704cce94958f992c8724965c57d7
leonard percival <leonardpercival@gmail.com>
2020-02-11 13:34:38 +0000

    removed subunit

--- app/javascript/components/form/errorRemover.js
+++ app/javascript/components/form/errorRemover.js
@@ -1,12 +1,18 @@
 export const errorRemover = () => {
   document.querySelectorAll('.req').forEach(ele => {
     ele.addEventListener("change", (e) => {
-      document.getElementById(e.currentTarget.dataset.error).remove()
+      let error = document.getElementById(e.currentTarget.dataset.error)
+      if (error) {
+        error.remove()
+      }
     })
   })
   document.querySelectorAll('.dep').forEach(ele => {
     ele.addEventListener("change", (e) => {
-      document.getElementById("departure-error").remove()
+      let error = document.getElementById("departure-error")
+      if (error) {
+        error.remove()
+      }
     })
   })
 }
