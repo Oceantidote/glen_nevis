6a864d232f74816fc1c44644713f9323aff9cfe5
leonard percival <leonardpercival@gmail.com>
2020-02-12 12:04:48 +0000

    email validations

--- app/javascript/components/form/frontEndValidator.js
+++ app/javascript/components/form/frontEndValidator.js
@@ -8,10 +8,17 @@ export const frontEndValidator = () => {
         blank.push(element)
       }
     })
+    let email = false
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('email').value)) {
+      email = true
+    }
     const gdpr = document.getElementById('gdpr').value == "true"
-    if (gdpr && blank.length == 0) {
+    if (gdpr && blank.length == 0 && email) {
       $('#quick_book').trigger('submit')
     } else {
+      if (!email) {
+        document.getElementById('email-error').innerHTML = "Invalid email format"
+      }
       blank.forEach((ele) => {
         let error = document.getElementById(ele.dataset.error)
         error.innerHTML = "Can't be blank"
