3523e5d96681d8644c3ec3b746d16b7a20adec9d
Ben Wright <ben1991wright@hotmail.co.uk>
2020-02-19 11:22:15 +0000

    remove checkboxes, may have released code ghosts

--- app/javascript/components/form/updateInput.js
+++ app/javascript/components/form/updateInput.js
@@ -2,17 +2,18 @@ export const updateInput = () => {
   const input = $('#extras-input')
   let string = ''
   document.querySelectorAll('.addon').forEach(ele => {
-    if (ele.childNodes[1].childNodes[0].checked) {
+    if (ele.querySelector('.addon-select').value !== '0') {
       if (
-        ele.childNodes[0].childNodes[0].innerHTML == 'Booking Fee' ||
-        ele.childNodes[0].childNodes[0].innerHTML == 'Type of unit' ||
-        ele.childNodes[0].childNodes[0].innerHTML == 'Cleaning Charge'
+        ele.querySelector('.addon-name').innerHTML == 'Booking Fee' ||
+        ele.querySelector('.addon-name').innerHTML == 'Type of unit' ||
+        ele.querySelector('.addon-name').innerHTML == 'Tent Size' ||
+        ele.querySelector('.addon-name').innerHTML == 'Cleaning Charge'
       ) {
         let new_string =
           '|' +
           ele.dataset.id +
           ',' +
-          ele.childNodes[1].childNodes[1].value +
+          ele.querySelector('.addon-select').value +
           ',' +
           ele.dataset.rate / 100
         string += new_string
@@ -21,7 +22,7 @@ export const updateInput = () => {
           '|' +
           ele.dataset.id +
           ',' +
-          ele.childNodes[1].childNodes[1].value * $('#nights').val() +
+          ele.querySelector('.addon-select').value * $('#nights').val() +
           ',' +
           ele.dataset.rate / 100
         string += new_string
