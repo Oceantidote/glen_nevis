221678fe6227c59736119c660d87fe6ca055c6d9
John Cameron <john@JC-Mac-5.local>
2020-07-27 17:38:50 +0100

    show day of week

--- app/javascript/components/form/dateHandler.js
+++ app/javascript/components/form/dateHandler.js
@@ -6,8 +6,8 @@ import { discountHandler } from './discountHandler'
 
 export const dateHandler = () => {
   flatpickr('#arrival_date', {
-    dateFormat: 'd/m/Y',
-    minDate: Date.now()
+    dateFormat: 'D d/m/Y',
+    minDate: Date.now() - (24*60*60*1000)*7
   })
   updateDeparture()
   $('#arrival_cal').change(updateDeparture)
@@ -24,10 +24,9 @@ const updateDeparture = () => {
   const arrival_element = document.getElementById('arrival_date')
   const nights = document.getElementById('nights').value
   const departure_element = document.getElementById('departure_date')
-  const arrival_date = moment(arrival_element.value, 'DD/MM/YYYY')
+  const arrival_date = moment(arrival_element.value.substring(4,14), 'DD/MM/YYYY')
   if (isNaN(arrival_date)) {
     return
   }
-  departure_element.value = arrival_date.format('YYYY-MM-DD')
-  departure_element.stepUp(nights)
+  departure_element.value = arrival_date.add(nights, 'days').format('ddd DD/MM/YYYY')
 }
